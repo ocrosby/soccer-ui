630abae55008c3262e565004ee5150dc22f6ad45
Omar Crosby <omar.crosby@gmail.com>
2022-07-21 09:49:58 -0400

    feat(pipes): Adding truncate and titlecase pipes.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SpinnerOverlayComponent } from './spinner-overlay/spinner-overlay.component';
 import { MaterialModule } from '../material.module';
 import { ClarityModule } from "@clr/angular";
+import { TitleCasePipe } from './title-case.pipe';
+import { TruncatePipe } from './truncate.pipe';
 @NgModule({
-    declarations: [SpinnerOverlayComponent],
+    declarations: [SpinnerOverlayComponent, TitleCasePipe, TruncatePipe],
     imports: [CommonModule, ClarityModule, MaterialModule],
-    exports: [SpinnerOverlayComponent]
+    exports: [SpinnerOverlayComponent, TitleCasePipe, TruncatePipe]
 })
 export class SharedModule {}
---
+++ src/app/shared/title-case.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'titlecase',
+})
+export class TitleCasePipe implements PipeTransform {
+    transform(value: string): string {
+        let first = value.substr(0, 1).toUpperCase();
+        return first + value.substr(1);
+    }
+}
